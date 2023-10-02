c41842abc98dec0005d9ccc6351deda25f8890f7
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-10-02 20:05:02 +0530

    Final backend commit - implemented Data Retrieval, Data analysis, Resposes, Blog search end point, Error handling, and Caching mechanism

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,64 @@
 const express = require('express');
-const app = express();
+const axios = require('axios');
+const _ = require('lodash');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const data = async () => {
+    try {
+        const response = await axios.get(process.env.HASURA_API_URL, {
+            headers: {
+                'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to fetch blog data');
+    }
+};
+
+const memoData = _.memoize(data, () => 'blogData', 300000);
+
+app.get('/api/blog-stats', async (req, res) => {
+    try {
+        const blogData = await memoData();
+        const totalBlogs = blogData.blogs.length;
+        const longestTitleBlog = _.maxBy(blogData.blogs, blog => blog.title.length);
+        const privacyBlogs = _.filter(blogData.blogs, blog => {
+            return blog.title && blog.title.toLowerCase().includes('privacy');
+        });
+        const uniqueTitles = _.uniqBy(blogData.blogs, 'title').map(blog => blog.title).filter(Boolean);
+
+        const responseStats = {
+            totalBlogs,
+            longestTitle: longestTitleBlog ? longestTitleBlog.title : null,
+            privacyBlogsCount: privacyBlogs.length,
+            uniqueTitles
+        };
+        res.json(responseStats);
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-const PORT = 5000;
+app.get('/api/blog-search', async (req, res) => {
+  try {
+      const query = req.query.query.toLowerCase(); 
+      const blogData = await memoData();
+      const searchResults = blogData.blogs.filter(blog => {
+          return blog.title.toLowerCase().includes(query);
+      });
+      res.json({ searchResults });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on Port ${PORT}`);
 });
